35578c295f7cd4f8e2f1d1deeba2684a332f172d
Ann Sizemore <annsize@seas.upenn.edu>
2020-09-16 09:49:26 -0400

    added names to matfile

--- code/writeForJason.jl
+++ code/writeForJason.jl
@@ -35,6 +35,7 @@ save_dir = "$(homedir())/$(config["save_dir_forJason"])/$(NNODES)nodes"
 ### Locate data
 betti_files = filter(x->occursin("_bettis.jld",x), readdir(read_dir))
 betti_files = filter(x -> occursin(DATE_STRING, x), betti_files)
+sort!(betti_files)
 println("Located the following betti curve files:")
 for betti_file in betti_files
     println(betti_file)
@@ -44,6 +45,7 @@ model_names_bettis = [split(betti_file,"_bettis")[1] for betti_file in betti_fil
 
 bettiBar_files = filter(x->occursin("_bettiBars",x), readdir(read_dir))
 bettiBar_files = filter(x -> occursin(DATE_STRING, x), bettiBar_files)
+sort!(bettiBar_files)
 println("\nLocated the following bettiBar files:")
 for bettiBar_file in bettiBar_files
     println(bettiBar_file)
@@ -89,6 +91,7 @@ for nametag in nametags
     nuBar_all = zeros(NREPS,MAXDIM,nModels)
 
 
+
     # Handle the threshold and non threshold types separately
     if occursin("threshold", nametag)
 
@@ -100,6 +103,8 @@ for nametag in nametags
         bettiBar_all_postnoise = zeros(NREPS,MAXDIM,nModels)
         muBar_all_postnoise = zeros(NREPS,MAXDIM,nModels)
         nuBar_all_postnoise = zeros(NREPS,MAXDIM,nModels)
+
+        names_ordered = []
 
         for (i, betti_file_nametag) in enumerate(betti_files_nametag)
 
@@ -128,6 +133,8 @@ for nametag in nametags
             muBar_all_postnoise[:,:, i] = bettiBar_dict["muBarArray"]
             nuBar_all_postnoise[:,:, i] = bettiBar_dict["nuBarArray"]
 
+            append!(names_ordered, [bettiBar_file_base])
+
         end
 
         ### Checks
@@ -135,9 +142,11 @@ for nametag in nametags
         println(sum([bettiBar_all_prenoise...]))
         println(sum([bettiBar_all_postnoise...]))
 
+
+
         ### Save .mat file for jason
 
-        matwrite("$(save_dir)/all_betti_data_$(nametag).mat", Dict("betti_curve_array_all" => betti_curve_array_all, 
+        matwrite("$(save_dir)/all_betti_data_$(nametag)_$(DATE_STRING).mat", Dict("betti_curve_array_all" => betti_curve_array_all, 
                 "bettiBar_all" => bettiBar_all, 
                 "muBar_all" => muBar_all,
                 "nuBar_all" => nuBar_all,
@@ -146,9 +155,10 @@ for nametag in nametags
                 "nuBar_all_prenoise" => nuBar_all_prenoise,
                 "bettiBar_all_postnoise" => bettiBar_all_postnoise,
                 "muBar_all_postnoise" => muBar_all_postnoise,
-                "nuBar_all_postnoise" => nuBar_all_postnoise))
+                "nuBar_all_postnoise" => nuBar_all_postnoise,
+                "names_ordered" => names_ordered))
 
-        printstyled("\nFinished saving file to $(save_dir)/all_betti_data_$(nametag).mat\n", color=:green)
+        printstyled("\nFinished saving file to $(save_dir)/all_betti_data_$(nametag)_$(DATE_STRING).mat\n", color=:green)
 
 
 
@@ -157,6 +167,7 @@ for nametag in nametags
     else    # if threshold not in nametag...
 
         ## Read in all the data and create 4 mega arrays for betti curves, bettiBar, muBar, and nuBar values
+        names_ordered = []
 
         for (i, betti_file_nametag) in enumerate(betti_files_nametag)
 
@@ -176,6 +187,8 @@ for nametag in nametags
             muBar_all[:,:, i] = bettiBar_dict["muBarArray"]
             nuBar_all[:,:, i] = bettiBar_dict["nuBarArray"]
 
+            append!(names_ordered, [bettiBar_file_base])
+
 
         end
 
@@ -187,12 +200,13 @@ for nametag in nametags
 
         ### Save .mat file for jason
 
-        matwrite("$(save_dir)/all_betti_data_$(nametag).mat", Dict("betti_curve_array_all" => betti_curve_array_all, 
+        matwrite("$(save_dir)/all_betti_data_$(nametag)_$(DATE_STRING).mat", Dict("betti_curve_array_all" => betti_curve_array_all, 
                 "bettiBar_all" => bettiBar_all, 
                 "muBar_all" => muBar_all,
-                "nuBar_all" => nuBar_all))
+                "nuBar_all" => nuBar_all,
+                "names_ordered" => names_ordered))
 
-        printstyled("\nFinished saving file to $(save_dir)/all_betti_data_$(nametag).mat\n", color=:green)
+        printstyled("\nFinished saving file to $(save_dir)/all_betti_data_$(nametag)_$(DATE_STRING).mat\n", color=:green)
 
     end # ends if threshold 
 
