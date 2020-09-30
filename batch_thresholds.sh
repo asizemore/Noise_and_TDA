857f310214b55f351f57078c2c7c6f80358633fa
Ann Sizemore <annsize@seas.upenn.edu>
2020-09-30 13:06:30 -0400

    prepare for clique runs

--- batch_thresholds.sh
+++ batch_thresholds.sh
@@ -102,7 +102,7 @@ do
 
     if [[ "$graph" == *"$mydate"* ]] ; then
 
-        if [[ "$graph" == *"Triangle"* ]] ; then
+        if [[ "$graph" == *"clique"* ]] ; then
 
             echo $graph
             # Run PH and save PH
